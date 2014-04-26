3c7f81d9aede0bd9ad69fbcb0cf916bdabf10f4c
Robert Kowalski <rok@kowalski.gd>
2014-04-26 13:48:11 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,34 @@
+;(function ($, window) {
+
+var $votes = $('#votes')
+    $form = $('#form-karma');
+
+$form.on('submit', function (e) {
+  e.preventDefault();
+
+  $form
+    .find('button')
+    .text('Thanks!')
+    .prop('disabled', true);
+
+  $.ajax({
+    type: 'POST',
+    url: '/karmas',
+    dataType: 'json',
+    data: $form.serialize(),
+    success: function (data) {
+      // fake vote count raise - until we have websocket support
+      var count = +$votes.text();
+      count = count + 1;
+      $votes.text(count);
+    }
+  });
+});
+
+// Logo
+var $logo = $('#logo');
+$logo.on('hover', function () {
+  $logo.toggleClass('show-back');
+});
+
+})(jQuery, window);
